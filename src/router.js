928a964db70a1825242a99a8ffbe5d4c8895e186
Valerii Zhyla <orator9x@gmail.com>
2022-02-01 10:44:08 +0200

    Linter

--- src/router.js
+++ src/router.js
@@ -4,43 +4,15 @@ class Router {
   }
 
   serve(routeName) {
-    let routesWithSpecifiedName = this.routes.filter(function(route){
-      return route.path === routeName;
-    });
-    if (routesWithSpecifiedName.isEmpty) {
-      throw new Error(`Route with name ${routeName} does not exist`);
+    const routesWithSpecifiedName = this.routes.filter((route) => route.path === routeName);
+    if (routesWithSpecifiedName.length === 0) {
+      throw new Error('Route does not exist');
     } else {
       return routesWithSpecifiedName[0];
     }
   }
-
-
 }
 
-function makeRouter(routes) {
+export default function makeRouter(routes) {
   return new Router(routes);
 }
-
-// import makeRouter from '@hexlet/code';
-//
-// const routes = [
-//   {
-//     // Роутер используется как часть на конкретном сайте, поэтому роутеру нужно знать лишь про сами маршруты на сайте
-//     // не учитываем протокол, хост и т. д.
-//     path: '/courses', // маршрут
-//     handler: () => 'courses!', // обработчик
-//   },
-//   {
-//     path: '/courses/basics',
-//     handler: () => 'basics',
-//   },
-// ];
-//
-// const router = makeRouter(routes);
-//
-// const path = '/courses';
-// const handler = router.serve(path);
-// handler(); // courses!
-//
-// // здесь выбросится исключение
-// router.serve('/no_such_way'); // Error
